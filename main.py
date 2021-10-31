21e3ebbc00df4a239fec53946524054cdeda2257
Ankit Sangwan <ankit.sangwan.5688@gmail.com>
2021-10-31 11:19:47 +0530

    few changes

--- main.py
+++ main.py
@@ -7,30 +7,25 @@ from mutagen.mp4 import MP4, MP4Cover
 import threading
 
 download_path = "./Songs" #Provide download path here
-def dwnld_sng(song_dwn_url, song_id):
-    with requests.get(song_dwn_url, stream=True) as r, open("{}/{}.m4a".format(download_path, song_id), "wb") as f:
+
+def dwnld_sng(song_dwn_url, song_title):
+    with requests.get(song_dwn_url, stream=True) as r, open("{}/{}.m4a".format(download_path, song_title), "wb") as f:
         file_size = int(r.headers['Content-Length'])
         for chunk in tqdm.tqdm(
         r.iter_content(chunk_size=1024),
         total= int(file_size / 1024),
         unit = 'KB',
-        desc = "Downloading {} by {}".format(song_name, artist_name),
+        desc = "Downloading {}".format(song_title),
         leave = True
         ):
             f.write(chunk)
 
-
-def rename(song_id, song_name, artist_name):
-    fname = "{}/{} - {}.m4a".format(download_path, song_name, artist_name)
-    os.rename("{}/{}.m4a".format(download_path, song_id), "{}".format(fname))
-
-def dwnld_img(image_url, img_name):
+def dwnld_img(image_url, img_path):
     response = requests.get(image_url)
-    with open(img_name, 'wb') as f:
+    with open(img_path, 'wb') as f:
         f.write(response.content)
 
-def save_metadata(song_id, song_name, artist_name, featured_artist, img_path, album, year, genre):
-    audio_path = "{}/{}.m4a".format(download_path, song_id)
+def save_metadata(audio_path, song_name, artist_name, featured_artist, img_path, album, year, genre):
     audio = MP4(audio_path)
     audio['\xa9nam'] = song_name
     audio['\xa9alb'] = album
@@ -47,7 +42,10 @@ def save_metadata(song_id, song_name, artist_name, featured_artist, img_path, al
         ]
     audio.save()
 
-if __name__ == "__main__":
+def main():
+    isdir = os.path.isdir(download_path)
+    if not isdir:
+        os.mkdir(download_path)
     while True:
         song_name = input("Enter Song Name: ")
         if song_name == "":
@@ -64,43 +62,40 @@ if __name__ == "__main__":
                 pref = input("Enter song number(Hit Enter for Default i,e 1): ").replace(".", '')
                 if pref == '':
                     pref = 1
-                else:
+                elif pref.isnumeric and pref <= len(song_data):
                     pref = int(pref)
-                try:
-                    if pref > len(song_data):
-                        print("Number out of range. Please Enter valid number")
-                    else:
-                        song_id = song_data[pref-1]['id']
-                        #print(song_id)
-                        song_data = jiosaavn.get_song(str(song_id))
-                        #print(song_data)
-                        song_dwn_url = song_data['media_url']
-                        song_name = song_data['song'].replace('?', '').strip()
-                        artist_name = song_data['primary_artists']
-                        featured_artist = song_data['featured_artists']
-                        album = song_data['album']
-                        year = song_data['year']
-                        lang = (song_data['language'])
-                        lang = lang[0].upper() + lang[1:]
-                        print("Found {} by {}".format(song_name, artist_name))
-                        image_url = song_data['image']
-                        img_path = "{}/{}.jpg".format(download_path, song_id)
-                        #print("Downloading Image")
-                        t1 = threading.Thread(target=dwnld_img, args=(image_url, img_path))
-                        t1.start()
-                        dwnld_sng(song_dwn_url, song_id)
-                        
-                        fname = "{}/{} - {}.m4a".format(download_path, artist_name, song_name)
-                        t1.join()
-                        #print("Saving matadata")
-                        save_metadata(song_id, song_name, artist_name, featured_artist, img_path, album, year, genre=lang)
-                        rename(song_id, song_name, artist_name)
-                        os.remove(img_path)
+                else:
+                    print("Invalid Value Entered")
+                    continue
+
+                song_id = song_data[pref-1]['id']
+                song_data = jiosaavn.get_song(str(song_id))
+                song_name = song_data['song'].replace('?', '').strip()
+                artist_name = song_data['primary_artists']
+                featured_artist = song_data['featured_artists']
+                song_title = "{} - {}".format(song_name, artist_name)
+                audio_path = "{}/{}.m4a".format(download_path, song_title)
+                img_path = "{}/{}.jpg".format(download_path, song_title)
+
+                album = song_data['album']
+                year = song_data['year']
+                lang = (song_data['language'])
+                lang = lang[0].upper() + lang[1:]
+                image_url = song_data['image']
                 
-                except Exception as e:
-                    print("Error: {}".format(e))
-                
-                
+                print("Selected {} by {}".format(song_name, artist_name))
+
+                t1 = threading.Thread(target=dwnld_img, args=(image_url, img_path))
+                t1.start()
+                dwnld_sng(song_dwn_url = song_data['media_url'], song_title = song_title)
+                fname = "{}/{} - {}.m4a".format(download_path, artist_name, song_name)
+                t1.join()
+                #print("Saving matadata")
+                save_metadata(audio_path = audio_path, song_name = song_name, artist_name = artist_name, featured_artist = featured_artist, img_path = img_path, album = album, year = year, genre = lang)
+                os.remove(img_path)
             except Exception as e:
                 print("Failed to Download Song")
                 print("ERROR: {}".format(e))
+
+if __name__ == "__main__":
+    main()
