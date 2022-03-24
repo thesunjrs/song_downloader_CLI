ab550255822cd2db8bad0f77c2b0fe0d12c05430
Hemish <hemish04082005@gmail.com>
2022-03-24 21:13:35 +0530

    New features, better cross platform support

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # song_downloader
-Downloads best quality (320kbps) songs with all metadata
+Downloads best quality (320kbps) songs with all metadata  
+
+All the parameters are optional.
+
+- If directory paramter is provided, all songs are downloaded in that directory. If the parameter is not provided, all songs are downloaded in current directory.
+- If file parameter is given, then it reads the names of songs from file. If this parameter is not provided, it defaults to interactive mode, asking name of new song each time after comopleting of download of previous song.
+- This program searches the song name provided and asks from a list of song which song the user wants to download by default, be it file mode or interactive mode. But if you dont want to be asked everytime to pick from the songs, you can add ```--dont-ask`` or ```-a``` flag.
+- Downloaded songs are named in this format: ```Title - Artist``` by default. But if you want to have the file name the same as name/query you provided, you can do so by passing ```--query-as-filename``` or ```-q``` flag.  
+
+This means if you just type ```python main.py```, it would ask you for name of song, and then ask you to pick one from search, it would download it, and this would be repeated. To exit, just press Ctrl+C.
+
+# Contributors
+Ankit Sangwan (initial author)
+Hemish (Contributor) <hemish04082005@gmail.com>
--- main.py
+++ main.py
@@ -5,18 +5,17 @@ import jiosaavn
 import os
 from mutagen.mp4 import MP4, MP4Cover
 import threading
+import argparse
 
-download_path = "D:\Songs\Songs" #Provide download path here
-
-def dwnld_sng(song_dwn_url, song_title):
-    with requests.get(song_dwn_url, stream=True) as r, open("{}/{}.m4a".format(download_path, song_title), "wb") as f:
+def dwnld_sng(song_dwn_url, song_title, download_path):
+    with requests.get(song_dwn_url, stream=True) as r, open(os.path.join(download_path, song_title)+".m4a", "wb") as f:
         file_size = int(r.headers['Content-Length'])
         for chunk in tqdm.tqdm(
         r.iter_content(chunk_size=1024),
         total= int(file_size / 1024),
         unit = 'KB',
         desc = "Downloading {}".format(song_title),
-        leave = True
+        leave = True,
         ):
             f.write(chunk)
 
@@ -43,41 +42,93 @@ def save_metadata(audio_path, song_name, artist_name, featured_artist, img_path,
     audio.save()
 
 def main():
-    isdir = os.path.isdir(download_path)
+    # Adding arguments for parsing
+    parser = argparse.ArgumentParser(description="Download songs with metadata in 320kbps")
+    parser.add_argument("--directory", '-d', required=False, nargs=1, metavar='/path/to/directory/in/os/specific/format')
+    parser.add_argument("--file", '-f', required=False, nargs=1, metavar='path/to/file/containing/names/of/songs')
+    parser.add_argument("--dont-ask", '-a', action='store_true', default=False, help="Do not ask which song to download from search and automatically pick the first entry")
+    parser.add_argument("--query-as-filename", '-q', action='store_true', default=False, help="The filenames of downloaded songs would be same as name/query given, instead of general 'Title - Artist' format")
+
+    args = parser.parse_args()
+
+
+    download_path = args.directory
+    if args.directory == None: # If no directory arguement is provided, pick current directory
+        download_path = os.getcwd()
+    else: # else pick the specified directory
+        download_path = download_path[0]
+
+    # if the directory does not exist, then create it
+    isdir = (os.path.isdir(download_path) and os.path.exists(download_path))
     if not isdir:
         os.mkdir(download_path)
+
+    if args.file == None: # No file arguement provided means we wanna run in interactive mode
+        mode = 'interactive' # ask for queries in terminal input in wile loop
+    else:
+        mode = 'file' # Dont ask for songs name everytime and pick names from a specified file
+    
+    if mode == 'file':
+        if not os.path.exists(args.file[0]):
+            raise "No such file {} present".format(args.file) # Raising exception if provided file doesnt exist
+        f = open(args.file[0], "rt") # Opening the provided filepath
+        length_of_file = len(f.readlines())
+        x = 0 # Counter variable which stores what line of file we have reached in while loop
+        f.close()
+        f = open(args.file[0], "rt") # again opening so that readline again starts from first line
+
+
     while True:
+        if mode == 'interactive':
+            song_name = ''
+            try:
+                song_name = input("Enter name of song: ")
+            except KeyboardInterrupt:
+                print("\nExiting program")
+                break
+        elif mode == 'file':
+            if x < length_of_file: # Lenght of file have length but x is index which starts form 0, thus less than sign, if it becomes equal: that means that we have reached end of file
+                song_name = str(f.readline()).strip()
+                x += 1
+            else:
+                break
         try:
-            song_name = input("Enter Song Name: ")
+            initial_query_name_given = song_name
             if song_name == "":
                 continue
             else:
                 try:
                     song_data = jiosaavn.search_for_song(song_name.replace('with lyrics', '').strip())
-                    print("Which one of these you want?")
-                    for i in range(len(song_data)):
-                        song_name = song_data[i]['title']
-                        subtitle = song_data[i]['description']
-                        pre = "{}. ".format(i+1)
-                        print("{}{}\n{}{}".format(pre, song_name, ' '*len(pre), subtitle))
+                    if not args.dont_ask: # Asking for which song to pick if dont ask is disabled (default)
+                        print("Which one of these you want?")
+                        for i in range(len(song_data)):
+                            song_name = song_data[i]['title']
+                            subtitle = song_data[i]['description']
+                            pre = "{}. ".format(i+1)
+                            print("{}{}\n{}{}".format(pre, song_name, ' '*len(pre), subtitle))
 
-                    pref = input("Enter song number(Hit Enter for Default i,e 1): ").replace(".", '')
-                    if pref == '':
-                        pref = 1
-                    elif pref.isnumeric and int(pref) <= len(song_data):
-                        pref = int(pref)
+                        pref = input("Enter song number(Hit Enter for Default i,e 1): ").replace(".", '')
+                        if pref == '':
+                            pref = 1
+                        elif pref.isnumeric and int(pref) <= len(song_data):
+                            pref = int(pref)
+                        else:
+                            print("Invalid Value Entered")
+                            continue
+
+                        song_id = song_data[pref-1]['id']
                     else:
-                        print("Invalid Value Entered")
-                        continue
-
-                    song_id = song_data[pref-1]['id']
+                        song_id = song_data[0]['id'] # Picking the first entry if dont ask is enabled
                     song_data = jiosaavn.get_song(str(song_id))
                     song_name = song_data['song'].replace('?', '').replace(':','').strip()
                     artist_name = song_data['primary_artists']
                     featured_artist = song_data['featured_artists']
-                    song_title = "{} - {}".format(song_name, artist_name)
-                    audio_path = "{}/{}.m4a".format(download_path, song_title)
-                    img_path = "{}/{}.jpg".format(download_path, song_title)
+                    if not args.query_as_filename: # Default behaviour
+                        song_title = "{} - {}".format(song_name, artist_name)
+                    else:
+                        song_title = initial_query_name_given # If the user wants the name of file to be same as query name
+                    audio_path = os.path.join(download_path, song_title)+".m4a"
+                    img_path = os.path.join(download_path, song_title) + ".jpg"
 
                     album = song_data['album']
                     year = song_data['year']
@@ -89,8 +140,11 @@ def main():
 
                     t1 = threading.Thread(target=dwnld_img, args=(image_url, img_path))
                     t1.start()
-                    dwnld_sng(song_dwn_url = song_data['media_url'], song_title = song_title)
-                    fname = "{}/{} - {}.m4a".format(download_path, artist_name, song_name)
+                    dwnld_sng(song_dwn_url = song_data['media_url'], song_title = song_title, download_path=download_path)
+                    if not args.query_as_filename:
+                        fname = os.path.join(download_path, "{} - {}.m4a".format(artist_name, song_name))
+                    else:
+                        fname = initial_query_name_given+".m4a"
                     t1.join()
                     #print("Saving matadata")
                     save_metadata(audio_path = audio_path, song_name = song_name, artist_name = artist_name, featured_artist = featured_artist, img_path = img_path, album = album, year = year, genre = lang)
